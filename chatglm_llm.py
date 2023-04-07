dfe966ed41ae1fa41b7f7731aa6d266bd0bb1322
littlepanda0716 <littlepanda0716@gmail>
2023-04-07 09:58:44 +0800

    update chatglm_llm.py

--- chatglm_llm.py
+++ chatglm_llm.py
@@ -15,9 +15,6 @@ model = (
     .cuda()
 )
 
-tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
-model = AutoModel.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True).half().cuda()
-
 class ChatGLM(LLM):
     max_token: int = 10000
     temperature: float = 0.1
