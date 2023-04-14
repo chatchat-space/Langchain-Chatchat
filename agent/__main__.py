57efaa54924ff7edfc5f1e54877596a80f69f5e4
glide-the <2533736852@qq.com>
2023-04-14 22:15:02 +0800

    chatglm_with_shared_memory_openai_llm:这个demo,基于openai的任务处理，实现本地文… …档调用链的示例，可以使用chatgml处理一部分任务事件，也可以使用chatgml来完成记忆共享（SharedMemory）,至于为什么使用openai原因主要有两个现象,第一点,因为大模型chatgml受限，导致很多langchain的demo只能部分实现，跟群主聊了下，这种问题只能通过微调模型，增加一部分指令集和状态术语的数据集进去，微调方案只能大家自己尝试了，对显卡要去较高.当然也可以考虑只训练一个任务格式化的gml,模型微调参考这个仓库的方式 https://github.com/liucongg/ChatGLM-Finetuning

---
+++ agent/__main__.py
@@ -0,0 +1,18 @@
+
+from .chatglm_with_shared_memory_openai_llm import *
+
+if __name__ == "__main__":
+    # 创建 ChatglmWithSharedMemoryOpenaiLLM 类的实例
+    chatglm_instance = ChatglmWithSharedMemoryOpenaiLLM()
+
+    # 使用代理链运行一些示例输入
+    chatglm_instance.agent_chain.run(input="我跟露露聊了什么?")
+    chatglm_instance.agent_chain.run(input="她开心吗?")
+    chatglm_instance.agent_chain.run(input="她有表达意见吗?")
+    chatglm_instance.agent_chain.run(input="根据历史对话总结下?")
+    chatglm_instance.agent_chain.run(input="""可以拓展下吗?，比如写个小作文。
+    大纲：游戏的美好回忆，触不可及的距离，不在乎得失
+    主题：露露的陪伴无比珍贵
+    背景：游戏，通话，当下
+    开篇需要以游戏相识你挑逗的话语讲起
+    """)
