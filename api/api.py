4df34ebaa05a24d8a05d0890681419c2d95049e0
tenggangren <tenggang@douyuxingchen.com>
2023-04-19 00:54:42 +0800

    处理引用错误

--- api/api.py
+++ api/api.py
@@ -15,7 +15,7 @@ from langchain.document_loaders import UnstructuredFileLoader
 # pip install pinecone-client,记得换源
 import pinecone
 import sentence_transformers
-from models import *
+from models.chatglm_llm import ChatGLM
 from configs import model_config as MODEL_CONFIG
 
 # 写到 import torch前面，否则多显卡情况有异常
