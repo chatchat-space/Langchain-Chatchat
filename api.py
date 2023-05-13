5acbb48d6aa11641dba55d0d447e6c65d98e77d9
royd <1615218238@qq.com>
2023-05-13 14:08:58 +0800

    Update api.py
    更改流式知识库对话为多轮对话（当前仅支持单轮），具体体现在websocket的传入参数增加history和knowledge_base_id

--- api.py
+++ api.py
@@ -2,23 +2,23 @@ import argparse
 import json
 import os
 import shutil
-import subprocess
-import tempfile
 from typing import List, Optional
 
 import nltk
 import pydantic
 import uvicorn
 from fastapi import Body, FastAPI, File, Form, Query, UploadFile, WebSocket
-from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing_extensions import Annotated
 from starlette.responses import RedirectResponse
 from chains.local_doc_qa import LocalDocQA
-from configs.model_config import (VS_ROOT_PATH, EMBEDDING_DEVICE, EMBEDDING_MODEL, LLM_MODEL, UPLOAD_ROOT_PATH,
-                                  NLTK_DATA_PATH, VECTOR_SEARCH_TOP_K, LLM_HISTORY_LEN)
+from configs.model_config import (VS_ROOT_PATH, UPLOAD_ROOT_PATH, EMBEDDING_DEVICE,
+                                  EMBEDDING_MODEL, LLM_MODEL, NLTK_DATA_PATH,
+                                  VECTOR_SEARCH_TOP_K, LLM_HISTORY_LEN, OPEN_CROSS_DOMAIN)
 
 nltk.data.path = [NLTK_DATA_PATH] + nltk.data.path
+
 
 class BaseResponse(BaseModel):
     code: int = pydantic.Field(200, description="HTTP status code")
@@ -85,8 +85,34 @@ def get_vs_path(local_doc_id: str):
 def get_file_path(local_doc_id: str, doc_name: str):
     return os.path.join(UPLOAD_ROOT_PATH, local_doc_id, doc_name)
 
-
 async def upload_file(
+        file: UploadFile = File(description="A single binary file"),
+        knowledge_base_id: str = Form(..., description="Knowledge Base Name", example="kb1"),
+):
+    saved_path = get_folder_path(knowledge_base_id)
+    if not os.path.exists(saved_path):
+        os.makedirs(saved_path)
+
+    file_content = await file.read()  # 读取上传文件的内容
+
+    file_path = os.path.join(saved_path, file.filename)
+    if os.path.exists(file_path) and os.path.getsize(file_path) == len(file_content):
+        file_status = f"文件 {file.filename} 已存在。"
+        return BaseResponse(code=200, msg=file_status)
+
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    vs_path = get_vs_path(knowledge_base_id)
+    vs_path, loaded_files = local_doc_qa.init_knowledge_vector_store([file_path], vs_path)
+    if len(loaded_files) > 0:
+        file_status = f"文件 {file.filename} 已上传至新的知识库，并已加载知识库，请开始提问。"
+        return BaseResponse(code=200, msg=file_status)
+    else:
+        file_status = "文件上传失败，请重新上传"
+        return BaseResponse(code=500, msg=file_status)
+
+async def upload_files(
         files: Annotated[
             List[UploadFile], File(description="Multiple files as UploadFile")
         ],
@@ -115,7 +141,7 @@ async def upload_file(
 
 
 async def list_docs(
-        knowledge_base_id: Optional[str] = Query(description="Knowledge Base Name", example="kb1")
+        knowledge_base_id: Optional[str] = Query(default=None, description="Knowledge Base Name", example="kb1")
 ):
     if knowledge_base_id:
         local_doc_folder = get_folder_path(knowledge_base_id)
@@ -169,7 +195,7 @@ async def delete_docs(
     return BaseResponse()
 
 
-async def chat(
+async def local_doc_chat(
         knowledge_base_id: str = Body(..., description="Knowledge Base Name", example="kb1"),
         question: str = Body(..., description="Question", example="工伤保险是什么？"),
         history: List[List[str]] = Body(
@@ -205,19 +231,45 @@ async def chat(
     )
 
 
-async def stream_chat(websocket: WebSocket, knowledge_base_id: str):
+async def chat(
+        question: str = Body(..., description="Question", example="工伤保险是什么？"),
+        history: List[List[str]] = Body(
+            [],
+            description="History of previous questions and answers",
+            example=[
+                [
+                    "工伤保险是什么？",
+                    "工伤保险是指用人单位按照国家规定，为本单位的职工和用人单位的其他人员，缴纳工伤保险费，由保险机构按照国家规定的标准，给予工伤保险待遇的社会保险制度。",
+                ]
+            ],
+        ),
+):
+    for resp, history in local_doc_qa.llm._call(
+            prompt=question, history=history, streaming=True
+    ):
+        pass
+
+    return ChatMessage(
+        question=question,
+        response=resp,
+        history=history,
+        source_documents=[],
+    )
+
+
+async def stream_chat(websocket: WebSocket):
     await websocket.accept()
-    vs_path = os.path.join(VS_ROOT_PATH, knowledge_base_id)
-
-    if not os.path.exists(vs_path):
-        await websocket.send_json({"error": f"Knowledge base {knowledge_base_id} not found"})
-        await websocket.close()
-        return
-
-    history = []
     turn = 1
     while True:
-        question = await websocket.receive_text()
+        input_json = await websocket.receive_json()
+        question, history, knowledge_base_id = input_json[""], input_json["history"], input_json["knowledge_base_id"]
+        vs_path = os.path.join(VS_ROOT_PATH, knowledge_base_id)
+
+        if not os.path.exists(vs_path):
+            await websocket.send_json({"error": f"Knowledge base {knowledge_base_id} not found"})
+            await websocket.close()
+            return
+
         await websocket.send_json({"question": question, "turn": turn, "flag": "start"})
 
         last_print_len = 0
@@ -260,12 +312,29 @@ def main():
     args = parser.parse_args()
 
     app = FastAPI()
-    app.websocket("/chat-docs/stream-chat/{knowledge_base_id}")(stream_chat)
-    app.post("/chat-docs/chat", response_model=ChatMessage)(chat)
-    app.post("/chat-docs/upload", response_model=BaseResponse)(upload_file)
-    app.get("/chat-docs/list", response_model=ListDocsResponse)(list_docs)
-    app.delete("/chat-docs/delete", response_model=BaseResponse)(delete_docs)
+    # Add CORS middleware to allow all origins
+    # 在config.py中设置OPEN_DOMAIN=True，允许跨域
+    # set OPEN_DOMAIN=True in config.py to allow cross-domain
+    if OPEN_CROSS_DOMAIN:
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=["*"],
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+    app.websocket("/local_doc_qa/stream-chat/")(stream_chat)
+
     app.get("/", response_model=BaseResponse)(document)
+
+    app.post("/chat", response_model=ChatMessage)(chat)
+
+    app.post("/local_doc_qa/upload_file", response_model=BaseResponse)(upload_file)
+    app.post("/local_doc_qa/upload_files", response_model=BaseResponse)(upload_files)
+    app.post("/local_doc_qa/local_doc_chat", response_model=ChatMessage)(local_doc_chat)
+    app.get("/local_doc_qa/list_files", response_model=ListDocsResponse)(list_docs)
+    app.delete("/local_doc_qa/delete_file", response_model=BaseResponse)(delete_docs)
+
 
     local_doc_qa = LocalDocQA()
     local_doc_qa.init_cfg(
