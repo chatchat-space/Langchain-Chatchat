67e26fe730b3fd2ab013efa1809c7815a585e1ba
imClumsyPanda <littlepanda0716@gmail.com>
2023-05-21 22:30:28 +0800

    update model_config.py

--- configs/model_config.py
+++ configs/model_config.py
@@ -135,3 +135,9 @@ flagging username: {FLAG_USER_NAME}
 # 是否开启跨域，默认为False，如果需要开启，请设置为True
 # is open cross domain
 OPEN_CROSS_DOMAIN = False
+
+# Bing 搜索必备变量
+# 使用 Bing 搜索需要使用 Bing Subscription Key
+# 具体申请方式请见 https://learn.microsoft.com/en-us/bing/search-apis/bing-web-search/quickstarts/rest/python
+BING_SEARCH_URL = "https://api.bing.microsoft.com/v7.0/search"
+BING_SUBSCRIPTION_KEY = ""
