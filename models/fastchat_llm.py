b354bcfea502acb7bf3234ccc8933cac0ac47809
root <jv.liu@i1368.com>
2023-05-25 15:24:31 +0000

    fastchat支持流失输出

--- models/fastchat_llm.py
+++ models/fastchat_llm.py
@@ -1,5 +1,6 @@
 from abc import ABC
 import requests
+import json
 from typing import Optional, List
 from langchain.llms.base import LLM
 
@@ -9,9 +10,9 @@ from models.base import (BaseAnswer,
 
 
 class FastChatLLM(BaseAnswer, LLM, ABC):
-    max_token: int = 10000
-    temperature: float = 0.01
-    top_p = 0.9
+    max_token: int = 2048
+    temperature: float = 0
+    top_p = 0.3
     checkPoint: LoaderCheckPoint = None
     # history = []
     history_len: int = 10
@@ -42,10 +43,61 @@ class FastChatLLM(BaseAnswer, LLM, ABC):
                          history: List[List[str]] = [],
                          streaming: bool = False):
 
-        response = "fastchat 响应结果"
-        history += [[prompt, response]]
-        answer_result = AnswerResult()
-        answer_result.history = history
-        answer_result.llm_output = {"answer": response}
 
-        yield answer_result
+        host = "http://localhost:8000"
+        data = {
+              "model": "vicuna-7b-v1.1",
+              "prompt": prompt,
+              "max_tokens":512,
+              "temperature": self.temperature,
+              "stream": streaming
+            }
+
+        print(data)
+
+        import time
+        start = time.time()
+
+
+        r = requests.post('%s/v1/completions' % host, json=data, stream=streaming)
+
+        print("use time: %ss" % (time.time() - start))
+        print("streaming %s" % streaming)
+
+        if not streaming:
+            resp = r.json()
+            if not resp.get('choices'):
+                response = str(resp)
+            else:
+                response = resp['choices'][0]['text']
+            if history:
+                history += [[prompt, response]]
+            answer_result = AnswerResult()
+            answer_result.history = history
+            answer_result.llm_output = {"answer": response}
+
+            yield answer_result
+        else:
+            for line in r.iter_lines(decode_unicode=True):
+                if not line: continue
+                over = "[DONE]" in line
+                if not over:
+                    resp = json.loads(line[6:])
+                    if not resp.get('choices'):
+                        response = str(resp)
+                    else:
+                        response = resp['choices'][0]['text']
+
+                if not over and response:
+                    if history:
+                        history += [[prompt, response]]
+                        answer_result.history = history
+                    answer_result = AnswerResult()
+                    answer_result.llm_output = {"answer": response}
+
+                    yield answer_result
+
+                history = []
+
+                if over:
+                    break
