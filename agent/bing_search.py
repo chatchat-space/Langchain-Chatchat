eb620ddefc7f2b92047cc7404308e08d428c7992
hzg0601 <hzg0601@163.com>
2023-06-12 16:22:07 +0800

    修复 bing_search.py的typo;更新model_config.py中Bing Subscription Key申请方式及注意事项

--- agent/bing_search.py
+++ agent/bing_search.py
@@ -7,7 +7,7 @@ from configs.model_config import BING_SEARCH_URL, BING_SUBSCRIPTION_KEY
 def bing_search(text, result_len=3):
     if not (BING_SEARCH_URL and BING_SUBSCRIPTION_KEY):
         return [{"snippet": "please set BING_SUBSCRIPTION_KEY and BING_SEARCH_URL in os ENV",
-                 "title": "env inof not fould",
+                 "title": "env info is not found",
                  "link": "https://python.langchain.com/en/latest/modules/agents/tools/examples/bing_search.html"}]
     search = BingSearchAPIWrapper(bing_subscription_key=BING_SUBSCRIPTION_KEY,
                                   bing_search_url=BING_SEARCH_URL)
