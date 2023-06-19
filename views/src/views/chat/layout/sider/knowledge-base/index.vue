ef5bab1d92d589f2711bc66f02b2befc68249b28
TOMCAT 007 <9116500+tomcat-007@user.noreply.gitee.com>
2023-06-19 21:16:51 +0800

    错误操作撤销了#653，补充提交

--- views/src/views/chat/layout/sider/knowledge-base/index.vue
+++ views/src/views/chat/layout/sider/knowledge-base/index.vue
@@ -3,7 +3,7 @@ import { NButton, NForm, NFormItem, NInput, NPopconfirm } from 'naive-ui'
 import { onMounted, ref } from 'vue'
 import filelist from './filelist.vue'
 import { SvgIcon } from '@/components/common'
-import { deletefile, getfilelist } from '@/api/chat'
+import { deletekb, getkblist } from '@/api/chat'
 import { idStore } from '@/store/modules/knowledgebaseid/id'
 const items = ref<any>([])
 const choice = ref('')
@@ -11,7 +11,7 @@ const store = idStore()
 
 onMounted(async () => {
   choice.value = store.knowledgeid
-  const res = await getfilelist({})
+  const res = await getkblist({})
   res.data.data.forEach((item: any) => {
     items.value.push({
       value: item,
@@ -52,8 +52,8 @@ const handleClick = () => {
   }
 }
 async function handleDelete(item: any) {
-  await deletefile(item.value)
-  const res = await getfilelist({})
+  await deletekb(item.value)
+  const res = await getkblist({})
   items.value = []
   res.data.data.forEach((item: any) => {
     items.value.push({
