72951d95fb09add14914064ff7f26441066ee707
zqt <1178747941@qq.com>
2023-08-07 16:32:34 +0800

    添加milvus库支持

--- server/knowledge_base/kb_service/milvus_kb_service.py
+++ server/knowledge_base/kb_service/milvus_kb_service.py
@@ -1,65 +1,79 @@
-from pymilvus import (
-    connections,
-    utility,
-    FieldSchema,
-    CollectionSchema,
-    DataType,
-    Collection,
-)
+from typing import List
 
-from server.knowledge_base.kb_service.base import KBService
+from langchain.embeddings.base import Embeddings
+from langchain.schema import Document
+from langchain.vectorstores import Milvus
 
-
-def get_collection(milvus_name):
-    return Collection(milvus_name)
+from configs.model_config import EMBEDDING_DEVICE, kbs_config
+from server.knowledge_base import KnowledgeFile
+from server.knowledge_base.kb_service.base import KBService, SupportedVSType, load_embeddings, add_doc_to_db
 
 
-def search(milvus_name, content, limit=3):
-    search_params = {
-        "metric_type": "L2",
-        "params": {"nprobe": 10},
-    }
-    c = get_collection(milvus_name)
-    return c.search(content, "embeddings", search_params, limit=limit, output_fields=["random"])
+class MilvusKBService(KBService):
+    milvus: Milvus
 
+    @staticmethod
+    def get_collection(milvus_name):
+        from pymilvus import Collection
+        return Collection(milvus_name)
 
-class MilvusKBService():
-    milvus_host: str
-    milvus_port: int
-    dim: int
-
-    def __init__(self, knowledge_base_name: str, vector_store_type: str, milvus_host="localhost", milvus_port=19530,
-                 dim=8):
-
-        super().__init__(knowledge_base_name, vector_store_type)
-        self.milvus_host = milvus_host
-        self.milvus_port = milvus_port
-        self.dim = dim
-
-    def connect(self):
-        connections.connect("default", host=self.milvus_host, port=self.milvus_port)
-
-    def create_collection(self, milvus_name):
-        fields = [
-            FieldSchema(name="pk", dtype=DataType.INT64, is_primary=True, auto_id=False),
-            FieldSchema(name="content", dtype=DataType.STRING),
-            FieldSchema(name="embeddings", dtype=DataType.FLOAT_VECTOR, dim=self.dim)
-        ]
-        schema = CollectionSchema(fields)
-        collection = Collection(milvus_name, schema)
-        index = {
-            "index_type": "IVF_FLAT",
+    @staticmethod
+    def search(milvus_name, content, limit=3):
+        search_params = {
             "metric_type": "L2",
-            "params": {"nlist": 128},
+            "params": {"nprobe": 10},
         }
-        collection.create_index("embeddings", index)
-        collection.load()
-        return collection
+        c = MilvusKBService.get_collection(milvus_name)
+        return c.search(content, "embeddings", search_params, limit=limit, output_fields=["content"])
 
-    def insert_collection(self, milvus_name, content=[]):
-        get_collection(milvus_name).insert(dataset)
+    def do_create_kb(self):
+        pass
+
+    def vs_type(self) -> str:
+        return SupportedVSType.MILVUS
+
+    def _load_milvus(self, embedding_device: str = EMBEDDING_DEVICE, embeddings: Embeddings = None):
+        _embeddings = embeddings
+        if _embeddings is None:
+            _embeddings = load_embeddings(self.embed_model, embedding_device)
+        self.milvus = Milvus(embedding_function=_embeddings,
+                             collection_name=self.kb_name, connection_args=kbs_config.get("milvus"))
+
+    def do_init(self):
+        self._load_milvus()
+
+    def do_drop_kb(self):
+        self.milvus.col.drop()
+
+    def do_search(self, query: str, top_k: int, embeddings: Embeddings) -> List[Document]:
+        self._load_milvus(embeddings=embeddings)
+        return self.milvus.similarity_search(query, top_k)
+
+    def add_doc(self, kb_file: KnowledgeFile):
+        """
+        向知识库添加文件
+        """
+        docs = kb_file.file2text()
+        self.milvus.add_documents(docs)
+        status = add_doc_to_db(kb_file)
+        return status
+
+    def do_add_doc(self, docs: List[Document], embeddings: Embeddings):
+        pass
+
+    def do_delete_doc(self, kb_file: KnowledgeFile):
+        filepath = kb_file.filepath.replace('\\', '\\\\')
+        delete_list = [item.get("pk") for item in
+                       self.milvus.col.query(expr=f'source == "{filepath}"', output_fields=["pk"])]
+        self.milvus.col.delete(expr=f'pk in {delete_list}')
+
+    def do_clear_vs(self):
+        self.milvus.col.drop()
 
 
 if __name__ == '__main__':
-    milvusService = MilvusService(milvus_host='192.168.50.128')
-    milvusService.insert_collection(test,dataset)
+    milvusService = MilvusKBService("test")
+    # milvusService.add_doc(KnowledgeFile("test.pdf", "test"))
+    # milvusService.delete_doc(KnowledgeFile("test.pdf", "test"))
+    milvusService.do_drop_kb()
+    print(milvusService.search_docs("测试"))
