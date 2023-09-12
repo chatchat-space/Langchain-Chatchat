efd6d4a251eb37ab92fa154fe0d0d464f67f382b
liunux4odoo <41217877+liunux4odoo@users.noreply.github.com>
2023-09-12 22:34:03 +0800

    修复milvus_kb_service中一些bug，添加文档后将数据同步到数据库 (#1452)

--- server/knowledge_base/migrate.py
+++ server/knowledge_base/migrate.py
@@ -68,9 +68,7 @@ def folder2db(
                 kb.add_doc(kb_file=kb_file, docs=docs, not_refresh_vs_cache=True)
             else:
                 print(result)
-
-        if kb.vs_type() == SupportedVSType.FAISS:
-            kb.save_vector_store()
+        kb.save_vector_store()
     elif mode == "fill_info_only":
         files = list_files_from_folder(kb_name)
         kb_files = file_to_kbfile(kb_name, files)
@@ -84,9 +82,7 @@ def folder2db(
 
         for kb_file in kb_files:
             kb.update_doc(kb_file, not_refresh_vs_cache=True)
-
-        if kb.vs_type() == SupportedVSType.FAISS:
-            kb.save_vector_store()
+        kb.save_vector_store()
     elif mode == "increament":
         db_files = kb.list_files()
         folder_files = list_files_from_folder(kb_name)
@@ -101,9 +97,7 @@ def folder2db(
                 kb.add_doc(kb_file=kb_file, docs=docs, not_refresh_vs_cache=True)
             else:
                 print(result)
-
-        if kb.vs_type() == SupportedVSType.FAISS:
-            kb.save_vector_store()
+        kb.save_vector_store()
     else:
         print(f"unspported migrate mode: {mode}")
 
@@ -133,8 +127,7 @@ def prune_db_files(kb_name: str):
         kb_files = file_to_kbfile(kb_name, files)
         for kb_file in kb_files:
             kb.delete_doc(kb_file, not_refresh_vs_cache=True)
-        if kb.vs_type() == SupportedVSType.FAISS:
-            kb.save_vector_store()
+        kb.save_vector_store()
         return kb_files
 
 def prune_folder_files(kb_name: str):
