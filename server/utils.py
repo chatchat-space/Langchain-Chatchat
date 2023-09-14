5855eadb489798ae6c9cb505c408caaab866954f
liunux4odoo <liunux@qq.com>
2023-09-14 09:04:26 +0800

    fix get_model_path

--- server/utils.py
+++ server/utils.py
@@ -214,18 +214,21 @@ def get_model_path(model_name: str, type: str = None) -> Optional[str]:
         for v in MODEL_PATH.values():
             paths.update(v)
 
-    if path_str := paths.get(model_name):
+    if path_str := paths.get(model_name): # 以 "chatglm-6b": "THUDM/chatglm-6b-new" 为例，以下都是支持的路径
         path = Path(path_str)
-        if path.is_dir(): # /xxx/xxx/chatglm-6b
+        if path.is_dir(): # 任意绝对路径
             return str(path)
 
         root_path = Path(MODEL_ROOT_PATH)
         if root_path.is_dir():
             path = root_path / model_name
-            if path.is_dir(): # {MODEL_ROOT_PATH}/chatglm-6b
+            if path.is_dir(): # use key, {MODEL_ROOT_PATH}/chatglm-6b
                 return str(path)
             path = root_path / path_str
-            if path.is_dir(): # {MODEL_ROOT_PATH}/THUDM/chatglm06b
+            if path.is_dir(): # use value, {MODEL_ROOT_PATH}/THUDM/chatglm-6b-new
+                return str(path)
+            path = root_path / path_str.split("/")[-1]
+            if path.is_dir(): # use value split by "/", {MODEL_ROOT_PATH}/chatglm-6b-new
                 return str(path)
         return path_str # THUDM/chatglm06b
 
