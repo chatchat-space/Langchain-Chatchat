cca46d64b41b22d94c4868c9dbede9bb69809bb0
liunux4odoo <liunux@qq.com>
2023-09-15 01:12:14 +0800

    給WEBUI设置主题

--- startup.py
+++ startup.py
@@ -360,7 +360,12 @@ def run_webui(started_event: mp.Event = None):
 
     p = subprocess.Popen(["streamlit", "run", "webui.py",
                           "--server.address", host,
-                          "--server.port", str(port)])
+                          "--server.port", str(port),
+                          "--theme.base", "light",
+                          "--theme.primaryColor", "#165dff",
+                          "--theme.secondaryBackgroundColor", "#f5f5f5",
+                          "--theme.textColor", "#000000",
+                        ])
     started_event.set()
     p.wait()
 
