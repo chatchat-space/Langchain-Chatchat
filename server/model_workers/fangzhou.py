9a7beef270e27ecea8c18cf264b9e5b7bee899d5
liunux4odoo <liunux@qq.com>
2023-09-17 00:21:13 +0800

    使火山方舟正常工作，添加错误处理和测试用例

--- server/model_workers/fangzhou.py
+++ server/model_workers/fangzhou.py
@@ -3,7 +3,50 @@ from configs.model_config import TEMPERATURE
 from fastchat import conversation as conv
 import sys
 import json
+from pprint import pprint
+from server.utils import get_model_worker_config
 from typing import List, Literal, Dict
+
+
+def request_volc_api(
+    messages: List[Dict],
+    model_name: str = "fangzhou-api",
+    version: str = "chatglm-6b-model",
+    temperature: float = TEMPERATURE,
+    api_key: str = None,
+    secret_key: str = None,
+):
+    from volcengine.maas import MaasService, MaasException, ChatRole
+
+    maas = MaasService('maas-api.ml-platform-cn-beijing.volces.com', 'cn-beijing')
+    config = get_model_worker_config(model_name)
+    version = version or config.get("version")
+    version_url = config.get("version_url")
+    api_key = api_key or config.get("api_key")
+    secret_key = secret_key or config.get("secret_key")
+
+    maas.set_ak(api_key)
+    maas.set_sk(secret_key)
+
+    # document: "https://www.volcengine.com/docs/82379/1099475"
+    req = {
+        "model": {
+            "name": version,
+        },
+        "parameters": {
+            # 这里的参数仅为示例，具体可用的参数请参考具体模型的 API 说明
+            "max_new_tokens": 1000,
+            "temperature": temperature,
+        },
+        "messages": messages,
+    }
+
+    try:
+        resps = maas.stream_chat(req)
+        for resp in resps:
+            yield resp
+    except MaasException as e:
+        print(e)
 
 
 class FangZhouWorker(ApiModelWorker):
@@ -21,7 +64,7 @@ class FangZhouWorker(ApiModelWorker):
             **kwargs,
     ):
         kwargs.update(model_names=model_names, controller_addr=controller_addr, worker_addr=worker_addr)
-        kwargs.setdefault("context_len", 16384)
+        kwargs.setdefault("context_len", 16384) # TODO: 不同的模型有不同的大小
         super().__init__(**kwargs)
 
         config = self.get_config()
@@ -41,40 +84,27 @@ class FangZhouWorker(ApiModelWorker):
 
     def generate_stream_gate(self, params):
         super().generate_stream_gate(params)
-        from volcengine.maas import MaasService, MaasException, ChatRole
-        maas = MaasService('maas-api.ml-platform-cn-beijing.volces.com', 'cn-beijing')
 
-        maas.set_ak(self.api_key)
-        maas.set_sk(self.secret_key)
-
-        # document: "https://www.volcengine.com/docs/82379/1099475"
-        req = {
-            "model": {
-                "name": self.version,
-            },
-            "parameters": {
-                # 这里的参数仅为示例，具体可用的参数请参考具体模型的 API 说明
-                # "max_new_tokens": 1000,
-                "temperature": params.get("temperature", TEMPERATURE),
-            },
-            "messages": [{"role": ChatRole.USER, "content": params["prompt"]}]
-        }
+        messages = self.prompt_to_messages(params["prompt"])
         text = ""
-        try:
 
-            resps = maas.stream_chat(req)
-            for resp in resps:
-                text += resp.choice.message.content
-                yield json.dumps({"error_code": 0, "text": text},
-                                 ensure_ascii=False).encode() + b"\0"
-        except MaasException as e:
-            print(e)
+        for resp in request_volc_api(messages=messages,
+                                    model_name=self.model_names[0],
+                                    version=self.version,
+                                    temperature=params.get("temperature", TEMPERATURE),
+                                    ):
+            error = resp.error
+            if error.code_n > 0:
+                data = {"error_code": error.code_n, "text": error.message}
+            elif chunk := resp.choice.message.content:
+                text += chunk
+                data = {"error_code": 0, "text": text}
+            yield json.dumps(data, ensure_ascii=False).encode() + b"\0"
 
-
-def get_embeddings(self, params):
-    # TODO: 支持embeddings
-    print("embedding")
-    print(params)
+    def get_embeddings(self, params):
+        # TODO: 支持embeddings
+        print("embedding")
+        print(params)
 
 
 if __name__ == "__main__":
@@ -84,8 +114,8 @@ if __name__ == "__main__":
 
     worker = FangZhouWorker(
         controller_addr="http://127.0.0.1:20001",
-        worker_addr="http://127.0.0.1:20006",
+        worker_addr="http://127.0.0.1:21005",
     )
     sys.modules["fastchat.serve.model_worker"].worker = worker
     MakeFastAPIOffline(app)
-    uvicorn.run(app, port=20006)
+    uvicorn.run(app, port=21005)
