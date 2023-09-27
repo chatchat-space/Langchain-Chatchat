d0279b836a96422b159973e9464ba18fa0c5e5c8
zR <2448370773@qq.com>
2023-09-27 21:09:18 +0800

    温度最小改成0，但是不应该支持负数

--- server/chat/chat.py
+++ server/chat/chat.py
@@ -21,7 +21,7 @@ async def chat(query: str = Body(..., description="用户输入", examples=["恼
                                        ),
                 stream: bool = Body(False, description="流式输出"),
                 model_name: str = Body(LLM_MODEL, description="LLM 模型名称。"),
-                temperature: float = Body(TEMPERATURE, description="LLM 采样温度", gt=0.0, le=1.0),
+                temperature: float = Body(TEMPERATURE, description="LLM 采样温度", ge=0.0, le=1.0),
                 # top_p: float = Body(TOP_P, description="LLM 核采样。勿与temperature同时设置", gt=0.0, lt=1.0),
                 prompt_name: str = Body("llm_chat", description="使用的prompt模板名称(在configs/prompt_config.py中配置)"),
          ):
