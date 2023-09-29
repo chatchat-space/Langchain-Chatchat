92212fbbcced7dcbe98d24639938dbf2d2c71172
zR <2448370773@qq.com>
2023-09-29 20:15:16 +0800

    按需修改

--- webui_pages/dialogue/dialogue.py
+++ webui_pages/dialogue/dialogue.py
@@ -121,7 +121,8 @@ def dialogue_page(api: ApiRequest):
                 kb_top_k = st.number_input("匹配知识条数：", 1, 20, VECTOR_SEARCH_TOP_K)
 
                 ## Bge 模型会超过1
-                score_threshold = st.slider("知识匹配分数阈值：", 0.0, 1.5, float(SCORE_THRESHOLD), 0.01)
+                score_threshold = st.slider("知识匹配分数阈值：", 0.0, 1.0, float(SCORE_THRESHOLD), 0.01)
+
                 # chunk_content = st.checkbox("关联上下文", False, disabled=True)
                 # chunk_size = st.slider("关联长度：", 0, 500, 250, disabled=True)
         elif dialogue_mode == "搜索引擎问答":
