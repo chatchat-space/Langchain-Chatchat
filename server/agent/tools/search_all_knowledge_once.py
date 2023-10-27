46b39e400976032f1db0af9167430cada071f0ad
zR <zRzRzRzRzRzRzR>
2023-10-27 13:12:47 +0800

    修复讯飞大模型token问题

--- server/agent/tools/search_all_knowledge_once.py
+++ server/agent/tools/search_all_knowledge_once.py
@@ -19,7 +19,7 @@ import json
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from server.chat.knowledge_base_chat import knowledge_base_chat
-from configs import VECTOR_SEARCH_TOP_K, SCORE_THRESHOLD
+from configs import VECTOR_SEARCH_TOP_K, SCORE_THRESHOLD, MAX_TOKENS
 
 import asyncio
 from server.agent import model_container
@@ -32,7 +32,7 @@ async def search_knowledge_base_iter(database: str, query: str):
                                          temperature=0.01,
                                          history=[],
                                          top_k=VECTOR_SEARCH_TOP_K,
-                                         max_tokens=None,
+                                         max_tokens=MAX_TOKENS,
                                          prompt_name="knowledge_base_chat",
                                          score_threshold=SCORE_THRESHOLD,
                                          stream=False)
