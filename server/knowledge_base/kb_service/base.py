248db4618778994bec233c24d260d935401472c9
glide-the <2533736852@qq.com>
2023-11-25 22:30:41 +0800

    Merge branch 'dev_summary' into dev_command_summary 实现summary_chunk 文档分段总结业务实现 使用 MapReduceDocumentsChain 生成摘要 # Conflicts: #	server/api.py #	server/knowledge_base/kb_doc_api.py #	server/knowledge_base/kb_service/base.py #	server/knowledge_base/migrate.py

--- server/knowledge_base/kb_service/base.py
+++ server/knowledge_base/kb_service/base.py
@@ -28,6 +28,7 @@ from typing import List, Union, Dict, Optional
 
 from server.embeddings_api import embed_texts
 from server.embeddings_api import embed_documents
+from server.knowledge_base.model.kb_document_model import DocumentWithVSId
 
 
 def normalize(embeddings: List[List[float]]) -> np.ndarray:
@@ -183,12 +184,22 @@ class KBService(ABC):
     def get_doc_by_ids(self, ids: List[str]) -> List[Document]:
         return []
 
-    def list_docs(self, file_name: str = None, metadata: Dict = {}) -> List[Document]:
+    def list_docs(self, file_name: str = None, metadata: Dict = {}) -> List[DocumentWithVSId]:
         '''
         通过file_name或metadata检索Document
         '''
         doc_infos = list_docs_from_db(kb_name=self.kb_name, file_name=file_name, metadata=metadata)
-        docs = self.get_doc_by_ids([x["id"] for x in doc_infos])
+        docs = []
+        for x in doc_infos:
+            doc_info_s = self.get_doc_by_ids([x["id"]])
+            if doc_info_s is not None and doc_info_s != []:
+                # 处理非空的情况
+                doc_with_id = DocumentWithVSId(**doc_info_s[0].dict(), id=x["id"])
+                docs.append(doc_with_id)
+            else:
+                # 处理空的情况
+                # 可以选择跳过当前循环迭代或执行其他操作
+                pass
         return docs
 
     @abstractmethod
