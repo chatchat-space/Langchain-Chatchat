269090ea66011eafe58e6c761949ff38979501ad
zR <2448370773@qq.com>
2024-01-12 12:01:22 +0800

    更新0.2.x Agent，之后的Agent在0.3.x更新

--- server/agent/tools/__init__.py
+++ server/agent/tools/__init__.py
@@ -3,7 +3,7 @@ from .search_knowledgebase_simple import search_knowledgebase_simple
 from .search_knowledgebase_once import search_knowledgebase_once, KnowledgeSearchInput
 from .search_knowledgebase_complex import search_knowledgebase_complex, KnowledgeSearchInput
 from .calculate import calculate, CalculatorInput
-from .weather_check import weathercheck, WhetherSchema
+from .weather_check import weathercheck, WeatherInput
 from .shell import shell, ShellInput
 from .search_internet import search_internet, SearchInternetInput
 from .wolfram import wolfram, WolframInput
