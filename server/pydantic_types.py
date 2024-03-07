87c912087cff635543f5da80229a77ff3cfdb224
glide-the <2533736852@qq.com>
2024-03-07 13:54:33 +0800

    修复glm3 agent被注释的agent会话文本结构解析代码 看起来输出的文本占位符如下，目前解析代码是有问题的 Thought <|assistant|> Action\r ```python tool_call(action_input) ```<|observation|>

--- server/pydantic_types.py
+++ server/pydantic_types.py
@@ -1,6 +1,7 @@
 from langchain_core.pydantic_v1 import *
 from pydantic.fields import FieldInfo
 from pydantic.schema import model_schema
+from pydantic.typing import typing
 
 # from pydantic.v1 import * 
 # from pydantic.v1.fields import FieldInfo
