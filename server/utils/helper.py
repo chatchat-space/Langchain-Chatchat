301c7c3d8c4e837d62eaed3c2d262eb0d3a93412
glide-the <2533736852@qq.com>
2024-03-13 20:16:42 +0800

    Revert "xinference的代码"

--- server/utils/helper.py
+++
@@ -1,9 +0,0 @@
-import pydantic
-from pydantic import BaseModel
-
-
-def dump_model(model: BaseModel) -> dict:
-    if hasattr(pydantic, 'model_dump'):
-        return pydantic.model_dump(model)
-    else:
-        return model.dict()
