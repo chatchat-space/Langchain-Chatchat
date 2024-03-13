7b2b24c0bca89a2f8397f5ac32cd754a66e8b8f7
dignfei <913015993@qq.com>
2024-03-13 10:36:14 +0800

    使用多进程提高导入知识库的速度 (#3276)

--- server/utils.py
+++ server/utils.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from pathlib import Path
 import asyncio
 import os
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from langchain.embeddings.base import Embeddings
+import sys
+import multiprocessing as mp
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_openai.llms import OpenAI
 import httpx
@@ -572,11 +573,36 @@ def run_in_thread_pool(
     tasks = []
     with ThreadPoolExecutor() as pool:
         for kwargs in params:
-            thread = pool.submit(func, **kwargs)
-            tasks.append(thread)
+            tasks.append(pool.submit(func, **kwargs))
 
         for obj in as_completed(tasks):
-            yield obj.result()
+            try:
+                yield obj.result()
+            except Exception as e:
+                logger.error(f"error in sub thread: {e}", exc_info=True)
+
+
+def run_in_process_pool(
+        func: Callable,
+        params: List[Dict] = [],
+) -> Generator:
+    '''
+    在线程池中批量运行任务，并将运行结果以生成器的形式返回。
+    请确保任务中的所有操作是线程安全的，任务函数请全部使用关键字参数。
+    '''
+    tasks = []
+    max_workers = None
+    if sys.platform.startswith("win"):
+        max_workers = min(mp.cpu_count(), 60) # max_workers should not exceed 60 on windows
+    with ProcessPoolExecutor(max_workers=max_workers) as pool:
+        for kwargs in params:
+            tasks.append(pool.submit(func, **kwargs))
+
+        for obj in as_completed(tasks):
+            try:
+                yield obj.result()
+            except Exception as e:
+                logger.error(f"error in sub process: {e}", exc_info=True)
 
 
 def get_httpx_client(
