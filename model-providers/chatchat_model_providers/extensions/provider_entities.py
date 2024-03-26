8ebfb34a51417c5c552d00e77a5c6218731aaa63
khazic <khazzz1c@gmail.com>
2024-03-26 14:49:26 +0800

    20240326

---
+++ model-providers/chatchat_model_providers/extensions/provider_entities.py
@@ -0,0 +1,37 @@
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel
+
+from model_providers.core.model_runtime.entities.model_entities import ModelType
+
+
+class RestrictModel(BaseModel):
+    model: str
+    base_model_name: Optional[str] = None
+    model_type: ModelType
+
+
+
+class CustomProviderConfiguration(BaseModel):
+    """
+    Model class for provider custom configuration.
+    """
+    credentials: dict
+
+
+class CustomModelConfiguration(BaseModel):
+    """
+    Model class for provider custom model configuration.
+    """
+    model: str
+    model_type: ModelType
+    credentials: dict
+
+
+class CustomConfiguration(BaseModel):
+    """
+    Model class for provider custom configuration.
+    """
+    provider: Optional[CustomProviderConfiguration] = None
+    models: list[CustomModelConfiguration] = []
