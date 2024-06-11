e7a5d6a5288062a2d4fd2294ad8aa026dd05b3fa
glide-the <2533736852@qq.com>
2024-06-11 15:14:58 +0800

    使用click增加配置中心子命令 (#4164)
    使用click增加配置中心子命令
    新增 ConfigModelWorkSpace

--- libs/chatchat-server/tests/unit_tests/config/test_config.py
+++ libs/chatchat-server/tests/unit_tests/config/test_config.py
@@ -1,6 +1,12 @@
 from pathlib import Path
 
-from chatchat.configs import ConfigBasicFactory, ConfigBasic, ConfigBasicWorkSpace
+from chatchat.configs import (
+    ConfigBasicFactory,
+    ConfigBasic,
+    ConfigBasicWorkSpace,
+    ConfigModelWorkSpace,
+    ConfigModel
+)
 import os
 
 
@@ -36,3 +42,56 @@ def test_workspace_default():
     assert LOG_FORMAT is not None
     assert LOG_PATH is not None
     assert MEDIA_PATH is not None
+
+
+def test_config_model_workspace():
+
+    config_model_workspace: ConfigModelWorkSpace = ConfigModelWorkSpace()
+
+    assert config_model_workspace.get_config() is not None
+
+    config_model_workspace.set_default_llm_model(llm_model="glm4")
+    config_model_workspace.set_default_embedding_model(embedding_model="text1")
+    config_model_workspace.set_agent_model(agent_model="agent")
+    config_model_workspace.set_history_len(history_len=1)
+    config_model_workspace.set_max_tokens(max_tokens=1000)
+    config_model_workspace.set_temperature(temperature=0.1)
+    config_model_workspace.set_support_agent_models(support_agent_models=["glm4"])
+    config_model_workspace.set_model_providers_cfg_path_config(model_providers_cfg_path_config="model_providers.yaml")
+    config_model_workspace.set_model_providers_cfg_host(model_providers_cfg_host="127.0.0.1")
+    config_model_workspace.set_model_providers_cfg_port(model_providers_cfg_port=8000)
+
+    config: ConfigModel = config_model_workspace.get_config()
+
+    assert config.DEFAULT_LLM_MODEL == "glm4"
+    assert config.DEFAULT_EMBEDDING_MODEL == "text1"
+    assert config.Agent_MODEL == "agent"
+    assert config.HISTORY_LEN == 1
+    assert config.MAX_TOKENS == 1000
+    assert config.TEMPERATURE == 0.1
+    assert config.SUPPORT_AGENT_MODELS == ["glm4"]
+    assert config.MODEL_PROVIDERS_CFG_PATH_CONFIG == "model_providers.yaml"
+    assert config.MODEL_PROVIDERS_CFG_HOST == "127.0.0.1"
+    assert config.MODEL_PROVIDERS_CFG_PORT == 8000
+    config_model_workspace.clear()
+
+
+def test_model_config():
+    from chatchat.configs import (
+        DEFAULT_LLM_MODEL, DEFAULT_EMBEDDING_MODEL, Agent_MODEL, HISTORY_LEN, MAX_TOKENS, TEMPERATURE,
+        SUPPORT_AGENT_MODELS, MODEL_PROVIDERS_CFG_PATH_CONFIG, MODEL_PROVIDERS_CFG_HOST, MODEL_PROVIDERS_CFG_PORT,
+        TOOL_CONFIG, MODEL_PLATFORMS, LLM_MODEL_CONFIG
+    )
+    assert DEFAULT_LLM_MODEL is not None
+    assert DEFAULT_EMBEDDING_MODEL is not None
+    assert Agent_MODEL is None
+    assert HISTORY_LEN is not None
+    assert MAX_TOKENS is None
+    assert TEMPERATURE is not None
+    assert SUPPORT_AGENT_MODELS is not None
+    assert MODEL_PROVIDERS_CFG_PATH_CONFIG is not None
+    assert MODEL_PROVIDERS_CFG_HOST is not None
+    assert MODEL_PROVIDERS_CFG_PORT is not None
+    assert TOOL_CONFIG is not None
+    assert MODEL_PLATFORMS is not None
+    assert LLM_MODEL_CONFIG is not None
