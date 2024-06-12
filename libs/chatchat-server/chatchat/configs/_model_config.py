f1546ad287c4530e1d4b9b6b04eec18597e8445c
glide-the <2533736852@qq.com>
2024-06-12 18:40:52 +0800

    更新版本

--- libs/chatchat-server/chatchat/configs/_model_config.py
+++ libs/chatchat-server/chatchat/configs/_model_config.py
@@ -132,25 +132,6 @@ class ConfigModelFactory(core_config.ConfigFactory[ConfigModel]):
                 "tts_models": [],
             },
 
-            {
-                "platform_name": "xinference",
-                "platform_type": "xinference",
-                "api_base_url": "http://127.0.0.1:9997/v1",
-                "api_key": "EMPTY",
-                "api_concurrencies": 5,
-                "llm_models": [
-                    "glm-4",
-                    "qwen2-instruct",
-                    "qwen1.5-chat",
-                ],
-                "embed_models": [
-                    "bge-large-zh-v1.5",
-                ],
-                "image_models": [],
-                "reranking_models": [],
-                "speech2text_models": [],
-                "tts_models": [],
-            },
 
         ]
         # 工具配置项
