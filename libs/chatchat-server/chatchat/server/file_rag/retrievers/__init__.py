c21faf74f026b8709412087695fd80f964271bf6
Lau Kwun Hang <101049505+kwunhang@users.noreply.github.com>
2024-07-18 21:59:40 +0800

    Fix Milvus retriever NotImplementedError (#4536)

--- libs/chatchat-server/chatchat/server/file_rag/retrievers/__init__.py
+++ libs/chatchat-server/chatchat/server/file_rag/retrievers/__init__.py
@@ -1,3 +1,4 @@
 from chatchat.server.file_rag.retrievers.base import BaseRetrieverService
 from chatchat.server.file_rag.retrievers.ensemble import EnsembleRetrieverService
 from chatchat.server.file_rag.retrievers.vectorstore import VectorstoreRetrieverService
+from chatchat.server.file_rag.retrievers.milvus_vectorstore import MilvusVectorstoreRetrieverService
