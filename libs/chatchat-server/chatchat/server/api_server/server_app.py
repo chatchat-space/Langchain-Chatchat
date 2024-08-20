a836f70f8f4ac6246c88d9a7d9f527ecef43b52d
hzg0601 <hzg0601@163.com>
2024-08-20 11:06:10 +0800

    style: unify name of api_server/routes

--- libs/chatchat-server/chatchat/server/api_server/server_app.py
+++ libs/chatchat-server/chatchat/server/api_server/server_app.py
@@ -46,7 +46,7 @@ def create_app(run_mode: str = None):
     app.include_router(openai_router)
     app.include_router(server_router)
     if Settings.model_settings.USE_RERANKER:
-        from chatchat.server.api_server.reranker_router import reranker_router
+        from chatchat.server.api_server.reranker_routes import reranker_router
         app.include_router(reranker_router)
     # 其它接口
     app.post(
