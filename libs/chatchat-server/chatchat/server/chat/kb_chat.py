a76c40f0f68d2577d87f74bf80e6f9905f8cea75
hzg0601 <hzg0601@163.com>
2024-08-31 16:54:53 +0800

    feat: add self_verify_evidence_one_call to support self-verify by only one call

--- libs/chatchat-server/chatchat/server/chat/kb_chat.py
+++ libs/chatchat-server/chatchat/server/chat/kb_chat.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-
+import re
+import copy
 import asyncio, json
 import uuid
 from typing import AsyncIterable, List, Optional, Literal
@@ -22,7 +23,7 @@ from chatchat.server.utils import (wrap_done, get_ChatOpenAI, get_default_llm,
                                    BaseResponse, get_prompt_template, build_logger,
                                    check_embed_model, api_address
                                 )
-
+from pprint import pprint
 
 logger = build_logger()
 
@@ -189,11 +190,11 @@ async def self_verify_evidence(
     prompt_zh = (
     "你将收到一个指令、证据、输出和可选的前述句子。如果给出了前述句子，输出应该是跟在这些前述句子后面的句子。"
     "你的任务是评估输出是否完全由证据中提供的信息支持。"
-    "使用以下蕴涵量表生成评分："
+    "使用以下蕴涵量表生成分数："
     "- [完全支持] - 输出中的所有信息都由证据或证据的提取支持。仅当输出和部分证据几乎相同时才适用。"
     "- [部分支持] - 输出在某种程度上由证据支持，但输出中有主要信息在证据中没有讨论。例如，如果指令询问两个概念，"
     "而证据只讨论其中一个，那么应该被视为[部分支持]。"
-    "- [无支持/矛盾] - 输出完全忽略证据，与证据无关，或与证据矛盾。"
+    "- [不支持/矛盾] - 输出完全忽略证据，与证据无关，或与证据矛盾。"
     "请确保不使用任何外部信息/知识来判断输出是否正确。只检查输出是否由证据支持，而不是检查输出是否遵循指令。"
     "以下是一个示例，帮助你更好地理解任务："
     "示例：\n\n"
@@ -207,14 +208,15 @@ async def self_verify_evidence(
     "当用作底层输入表示时，词和短语嵌入已被证明可以提高NLP任务的性能，例如句法分析、情感分析、下一个令牌预测以及类比检测。"
     "**评分**: [完全支持]"
     "解释：输出句子讨论了词嵌入的应用，证据提到了所有的应用，如句法分析、情感分析、下一个令牌预测以及类比检测。"
-    "因此，评分应该是[完全支持]。"
+    "因此，分数应该是[完全支持]。"
     "示例完成。\n\n"
-    "请按照以下格式提供你对证据的评分：[完全支持]、[部分支持]或[无支持/矛盾]。直接输出评分，不要添加任何内容！"
+    "请按照以下格式提供你对证据的评分：[完全支持]、[部分支持]或[不支持/矛盾]。直接输出评级，不要添加任何内容！"
     "指令：{}"
     "输出：{}"
     "证据：{}"
     "评分："
     )
+
 
     prompt = prompt_zh if lang == "zh" else prompt_en
     prompts = [prompt.format(query, answer, doc['page_content']) for doc in docs]
@@ -229,12 +231,157 @@ async def self_verify_evidence(
     results = await llm.abatch(messages)
     results_key = [result.content for result in results]
     logger.info(f"self_verify results_key: {results_key}")
-    filter_word = "完全支持" if lang == "zh" else "Fully supported"
-    support = [1 if filter_word in result else 0 for result in results_key]
+    ## -------keep fully support only---
+    # filter_word = "完全支持" if lang == "zh" else "Fully supported"
+    # support = [1 if filter_word in result else 0 for result in results_key]
+    ## ---- keep fully support and partial support ----
+    filter_word_ex = "不支持/矛盾" if lang == "zh" else "No support / Contradictory"
+    support = [0 if filter_word_ex in result else 1 for result in results_key]
+
     support_idx = [i for i, r in enumerate(support) if r == 1]
+
     docs_self_verify = [docs[i] for i in support_idx]
     return docs_self_verify
 
+async def self_verify_evidence_one_call(
+        docs:List[str],
+        answer:str,
+        model:str,
+        temperature:float,
+        max_tokens:int,
+        query:str,
+        lang="zh",
+        ):
+    """
+    select related documents accroding to answer by 
+    self-verify whether the evidence support the answer by only one call
+    docs: list of documents
+    answer: user answer
+    model: LLM model
+    temperature: LLM temperature
+    max_tokens: LLM max_tokens
+    query: user query
+    lang: language, "zh" or "en"
+    """
+    prompt_en = (
+    "You will receive an instruction, several evidences, and output, and optional preceding sentences. If the"
+    "preceding sentence is given, the output should be the sentence that follows those preceding"
+    "sentences. Your task is to evaluate if the output is fully supported or partial supported by the information provided"
+    "in the evidences, generate the scores and indides of evidences."
+    "Use the following entailment scale to generate a score:"
+    "- [Fully supported] - All information in output is supported by the evidences, or extractions"
+    "from the evidences. This is only applicable when the output and part of the evidences are"
+    "almost identical."
+    "- [Partially supported] - The output is supported by the evidences to some extent, but there"
+    "is major information in the output that is not discussed in the evidences. For example, if an"
+    "instruction asks about two concepts and the evidences only discusses either of them, it should"
+    "be considered a [Partially supported]."
+    "- [No support / Contradictory] - The output completely ignores evidences, is unrelated to the"
+    "evidences, or contradicts the evidences. This can also happen if the evidences are irrelevant to the"
+    "instruction."
+    "Make sure to not use any external information/knowledge to judge whether the output is true or not. "
+    "Only check whether the output is supported by the evidences, and not"
+    "whether the output follows the instructions or not."
+    "Here's one example to help you understand the task better:\n\n"
+    "Example:\n\n"
+    "Instruction: Explain the use of word embeddings in Natural Language Processing."
+    "Preceding sentences Word embeddings are one of the most powerful tools available for"
+    "Natural Language Processing (NLP). They are mathematical representations of words or"
+    "phrases in a vector space, allowing similarities between words and the context in which they"
+    "are used to be measured."
+    "Output Word embeddings are useful for tasks such as sentiment analysis, text classification,"
+    "predicting the next word in a sequence, and understanding synonyms and analogies."
+    "Evidence 1: Word embedding"
+    "Word embedding is the collective name for a set of language modeling and feature learning"
+    "techniques in natural language processing (NLP) where words or phrases from the vocabulary"
+    "are mapped to vectors of real numbers. Conceptually it involves a mathematical embedding"
+    "from a space with one dimension per word to a continuous vector space with a much lower"
+    "dimension. Methods to generate this mapping include neural networks, dimensionality"
+    "reduction on the word co-occurrence matrix, probabilistic models, explainable knowledge"
+    "base method, and explicit representation in terms of the context in which words appear. Word"
+    "and phrase embeddings, when used as the underlying input representation, have been shown"
+    "to boost the performance in NLP tasks such as syntactic parsing, sentiment analysis, next"
+    "token predictions as well and analogy detection.\n"
+    "Evidence 2: BERT"
+    "BERT language model is an open source machine learning framework for natural language processing (NLP). "
+    "BERT is designed to help computers understand the meaning of ambiguous language "
+    "in text by using surrounding text to establish context.\n"
+    "Evidence 3: GPT"
+    "GPT is a large language model that can generate human-like text. It is trained on a large corpus of text data "
+    "and can generate text that is similar to the text it was trained on.\n"
+    "Evidence 1: [Fully supported]"
+    "Evidence 2: [Partially supported]"
+    "Evidence 3: [No support / Contradictory]"
+    "Examples completed.\n\n"
+    "Please provide your response to the evidences in the following format: "
+    "Evidence index: [Fully supported], [Partially supported], or [No support / Contradictory]."
+
+    "Instruction: {}"
+    "Output: {}"
+    "Evidences:\n {}"
+    "Your response: "
+    )
+
+    prompt_zh = (
+    "你将收到一个指令、多个证据、输出和可选的前述句子。如果给出了前述句子，输出应该是跟在这些前述句子后面的句子。"
+    "你的任务是评估输出是否完全由证据支持或部分由证据支持，生成分数和证据的索引。"
+    "使用以下蕴涵量表生成分数："
+    "- [完全支持] - 输出中的所有信息都由证据或证据的提取支持。仅当输出和部分证据几乎相同时才适用。"
+    "- [部分支持] - 输出在某种程度上由证据支持，但输出中有主要信息在证据中没有讨论。例如，如果指令询问两个概念，"
+    "而证据只讨论其中一个，那么应该被视为[部分支持]。"
+    "- [不支持/矛盾] - 输出完全忽略证据，与证据无关，或与证据矛盾。"
+    "请确保不使用任何外部信息/知识来判断输出是否正确。只检查输出是否由证据支持，而不是检查输出是否遵循指令。"
+    "以下是一个示例，帮助你更好地理解任务："
+    "示例：\n\n"
+    "指令：解释自然语言处理中词嵌入的使用。"
+    "前述句子：词嵌入是自然语言处理（NLP）中最强大的工具之一。它们是词或短语在向量空间中的数学表示，"
+    "允许测量单词之间的相似性以及它们的上下文。"
+    "输出：词嵌入对情感分析、文本分类、预测序列中的下一个词以及理解同义词和类比等任务非常有用。"
+    "证据 1：词嵌入"
+    "词嵌入是自然语言处理（NLP）中一组语言建模和特征学习技术的统称，其中词汇表中的词或短语被映射到实数向量。"
+    "从概念上讲，它涉及从每个词一个维度的空间到一个具有更低维度的连续向量空间的数学嵌入。生成此映射的方法包括"
+    "神经网络、词共现矩阵的降维、概率模型、可解释的知识库方法以及根据单词出现的上下文的显式表示。"
+    "当用作底层输入表示时，词和短语嵌入已被证明可以提高NLP任务的性能，例如句法分析、情感分析、下一个令牌预测以及类比检测。\n"
+    "证据 2：BERT"
+    "BERT语言模型是一种用于自然语言处理（NLP）的开源机器学习框架。"
+    "BERT旨在通过使用周围文本来建立上下文，帮助计算机理解文本中的模糊语言的含义。\n"
+    "证据 3：GPT"
+    "GPT是一个可以生成类似人类文本的大型语言模型。它是在大量文本数据上训练的，可以生成与其训练文本相似的文本。\n"
+    "证据 1：[完全支持]"
+    "证据 2：[部分支持]"
+    "证据 3：[不支持/矛盾]"
+    "示例完成。\n\n"
+    "请按照以下格式提供你对证据的评分："
+    "证据索引：[完全支持]、[部分支持]或[不支持/矛盾]。不要添加任何额外内容！"
+    "指令：{}"
+    "输出：{}"
+    "证据：\n {}"
+    "你的回答："
+    )
+    prompt = prompt_zh if lang == "zh" else prompt_en
+    keyword = "证据" if lang == "zh" else "Evidence"
+    evidence = ""
+    for i, doc in enumerate(docs):
+        evidence += f"{keyword} {i+1}: {doc['page_content']}\n"
+    prompt = prompt.format(query, answer, evidence)
+    messages = [("human",prompt)]
+    llm = get_ChatOpenAI(
+                model_name=model,
+                temperature=temperature,
+                max_tokens=max_tokens,
+
+            )
+    result = llm.invoke(messages)
+    pprint(result)
+    result_key = result.content
+    pattern = re.compile(rf'{keyword} (\d+)：\[(.*?)\]')
+    result = pattern.findall(result_key)
+    pprint(result_key)
+    pprint(result)
+    filter_key_ex = "不支持/矛盾" if lang == "zh" else "No support / Contradictory"
+    supported_idex = [int(r[0])-1 for r in result if r[-1] != filter_key_ex]
+    docs_self_verify = [docs[i] for i in supported_idex]
+    return docs_self_verify
 
 
 async def kb_chat(query: str = Body(..., description="用户输入", examples=["你好"]),
@@ -269,6 +416,7 @@ async def kb_chat(query: str = Body(..., description="用户输入", examples=["
                 ),
                 return_direct: bool = Body(False, description="直接返回检索结果，不送入 LLM"),
                 request: Request = None,
+                lang: str = Body("zh", description="语言"),
                 ):
     start = time.time()
     if mode == "local_kb":
@@ -328,6 +476,43 @@ async def kb_chat(query: str = Body(..., description="用户输入", examples=["
             #     score_threshold=score_threshold,
             # ))
             # rich.print(docs)
+            # # 加入reranker
+            docs_original = copy.deepcopy(docs)
+            # * -----------------add reranker---------------------------- 
+            if Settings.model_settings.USE_RERANKER:
+                from chatchat.server.reranker.reranker import reranker_docs
+                docs = await reranker_docs(query, docs, top_k)
+            if Settings.kb_settings.ADAPTIVE_DOCUMENTS:
+                start = time.time()
+                docs = await adaptive_docs(
+                                        docs, 
+                                        model=model,
+                                        temperature=temperature,
+                                        max_tokens=max_tokens,
+                                        query=query,
+                                        lang=lang
+                                        )
+                end = time.time()
+                logger.info(f"adaptive_docs time: {end-start}s")
+            # filter evidence with self-verify evidence
+            if Settings.kb_settings.SELF_VERIFY_EVIDENCE:
+                start = time.time()
+                docs = await self_verify_evidence(
+                                        docs, 
+                                        answer=query,
+                                        model=model,
+                                        temperature=temperature,
+                                        max_tokens=max_tokens,
+                                        query=query,
+                                        lang=lang
+                                        )
+                end = time.time()
+                logger.info(f"self_verify_evidence time: {end-start}s")
+                
+            source_documents = format_reference(kb_name, 
+                                                docs, 
+                                                api_address(is_public=True), 
+                                                doc_source=doc_source)
             if return_direct:
                 yield OpenAIChatOutput(
                     id=f"chat{uuid.uuid4()}",
@@ -359,42 +544,7 @@ async def kb_chat(query: str = Body(..., description="用户输入", examples=["
 
 
             # TODO： 视情况使用 API
-            # # 加入reranker
-            # * -----------------add reranker---------------------------- 
-            if Settings.model_settings.USE_RERANKER:
-                from chatchat.server.reranker.reranker import reranker_docs
-                docs = await reranker_docs(query, docs, top_k)
-            if Settings.kb_settings.ADAPTIVE_DOCUMENTS:
-                start = time.time()
-                docs = await adaptive_docs(
-                                        docs, 
-                                        model=model,
-                                        temperature=temperature,
-                                        max_tokens=max_tokens,
-                                        query=query,
-                                        lang='zh'
-                                        )
-                end = time.time()
-                logger.info(f"adaptive_docs time: {end-start}s")
-            # filter evidence with self-verify evidence
-            if Settings.kb_settings.SELF_VERIFY_EVIDENCE:
-                start = time.time()
-                docs = await self_verify_evidence(
-                                        docs, 
-                                        answer=query,
-                                        model=model,
-                                        temperature=temperature,
-                                        max_tokens=max_tokens,
-                                        query=query,
-                                        lang='zh'
-                                        )
-                end = time.time()
-                logger.info(f"self_verify_evidence time: {end-start}s")
-                
-            source_documents = format_reference(kb_name, 
-                                                docs, 
-                                                api_address(is_public=True), 
-                                                doc_source=doc_source)
+
             context = "\n\n".join([doc["page_content"] for doc in docs])
 
             if len(docs) == 0:  # 如果没有找到相关文档，使用empty模板
@@ -452,6 +602,8 @@ async def kb_chat(query: str = Body(..., description="用户输入", examples=["
                     content=answer,
                     role="assistant",
                     model=model,
+                    docs=source_documents,
+                    docs_original=docs_original,
                 )
                 end = time.time()
                 logger.info(f"chat time: {end-start}s")
