0d74144483ee465b3a156210ba8b2bfc7257b0eb
glide-the <2533736852@qq.com>
2025-04-05 03:13:17 +0800

    代码检查、格式化

--- frontend/src/features/FolderPanel/index.tsx
+++ frontend/src/features/FolderPanel/index.tsx
@@ -1,64 +1,60 @@
 // @ts-nocheck
-import { Folder } from '@lobehub/icons';
-import { Button, Input, Modal } from 'antd';
-import { memo, useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { DraggablePanel, DraggablePanelContainer } from '@lobehub/ui';
+import { createStyles } from 'antd-style';
+import isEqual from 'fast-deep-equal';
+import { PropsWithChildren, memo, useState } from 'react';
 
-import { FormAction } from '@/features/Conversation/Error/style';
+import { FOLDER_WIDTH } from '@/const/layoutTokens';
 import { useGlobalStore } from '@/store/global';
-import { folderSelectors } from '@/store/global/selectors';
 
-const FolderPanel = memo(() => {
-  const { t } = useTranslation('error');
-  const [showModal, setShowModal] = useState(false);
+export const useStyles = createStyles(({ css, token }) => ({
+  panel: css`
+    height: 100%;
+    color: ${token.colorTextSecondary};
+    background: ${token.colorBgContainer};
+  `,
+}));
 
-  const [folderName, setConfig] = useGlobalStore((s) => [
-    folderSelectors.folderName(s),
-    s.setFolderConfig,
+const FolderPanel = memo<PropsWithChildren>(({ children }) => {
+  const { styles } = useStyles();
+  const [sessionsWidth, sessionExpandable, updatePreference] = useGlobalStore((s) => [
+    s.preference.sessionsWidth,
+    s.preference.showSessionPanel,
+    s.updatePreference,
   ]);
+  const [tmpWidth, setWidth] = useState(sessionsWidth);
+  if (tmpWidth !== sessionsWidth) setWidth(sessionsWidth);
 
   return (
-    <FormAction
-      avatar={<Folder size={56} />}
-      description={t('unlock.folder.Folder.description')}
-      title={t('unlock.folder.Folder.title')}
+    <DraggablePanel
+      className={styles.panel}
+      defaultSize={{ width: tmpWidth }}
+      expand={sessionExpandable}
+      maxWidth={400}
+      minWidth={FOLDER_WIDTH}
+      onExpandChange={(expand) => {
+        updatePreference({
+          sessionsWidth: expand ? 320 : 0,
+          showSessionPanel: expand,
+        });
+      }}
+      onSizeChange={(_, size) => {
+        if (!size) return;
+
+        const nextWidth = typeof size.width === 'string' ? Number.parseInt(size.width) : size.width;
+
+        if (isEqual(nextWidth, sessionsWidth)) return;
+
+        setWidth(nextWidth);
+        updatePreference({ sessionsWidth: nextWidth });
+      }}
+      placement="left"
+      size={{ height: '100%', width: sessionsWidth }}
     >
-      <Input
-        onChange={(e) => {
-          setConfig({ folderName: e.target.value });
-        }}
-        placeholder={'My Folder'}
-        type={'block'}
-        value={folderName}
-      />
-      <Button
-        onClick={() => {
-          setShowModal(true);
-        }}
-        type={'primary'}
-      >
-        {t('unlock.folder.createFolder')}
-      </Button>
-      <Modal
-        onCancel={() => {
-          setShowModal(false);
-        }}
-        onOk={() => {
-          setShowModal(false);
-        }}
-        open={showModal}
-        title={t('unlock.folder.createFolder')}
-      >
-        <Input
-          onChange={(e) => {
-            setConfig({ folderName: e.target.value });
-          }}
-          placeholder={'My Folder'}
-          type={'block'}
-          value={folderName}
-        />
-      </Modal>
-    </FormAction>
+      <DraggablePanelContainer style={{ flex: 'none', height: '100%', minWidth: FOLDER_WIDTH }}>
+        {children}
+      </DraggablePanelContainer>
+    </DraggablePanel>
   );
 });
 
