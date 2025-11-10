40f952dc893792647406caa9e3bd3ff03315ecbd
glide-the <2533736852@qq.com>
2025-11-10 13:53:00 +0800

    Refactor imports to use pydantic directly and update PYDANTIC_V2 definition in utils. This change improves clarity and consistency across the codebase.

--- libs/chatchat-server/langchain_chatchat/utils/__init__.py
+++ libs/chatchat-server/langchain_chatchat/utils/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 from langchain_chatchat.utils.history import History
+import pydantic
+PYDANTIC_V2 = pydantic.VERSION.startswith("2.")
 
-__all__ = ["History"]
+__all__ = ["History", "PYDANTIC_V2"]
